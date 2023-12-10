da026e8d99f945143b64ca83d3f06fc937097eef
kmr-rohit <rk972006@student.nitw.ac.in>
2023-12-10 09:25:30 +0530

    Added Speed Insights and Analytics

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,6 @@
 import type { Post } from "$lib/types"
 import { dev } from '$app/environment';
-import { inject } from '@vercel/analytics';
- 
-inject({ mode: dev ? 'development' : 'production' });
+
 export async function load({ fetch }) {
 	const response = await fetch('api/posts?latest=true')
 	const posts: Post[] = await response.json()
